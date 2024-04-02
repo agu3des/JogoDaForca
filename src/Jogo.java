3eb3484616bca5168694e53730df181308ddd711
Ananda <anandaguedesdoo@gmail.com>
2024-04-01 22:49:45 -0300

    alteracoes

---
+++ src/Jogo.java
@@ -0,0 +1,39 @@
+package ProjetoPoo.src;
+
+public class Jogo {
+	
+	
+	public String jogoDaForca() {
+		 	
+	}
+	public int getTamanho() {
+		
+	}
+	public String getDica() {
+		
+	}
+	public void iniciar() {
+		
+	}
+	public String getOcorrencias(string letraDigitada) {
+		
+	}
+	public int getAcertos() {
+		
+	}
+	public int getNumeroPenalidade() {
+		
+	}
+	public String getNomePenalidade() {
+		
+	}
+	public String getPalavraAdivinhada() {
+		
+	}
+	public int getResultado() {
+		
+	}
+	public void terminou() {
+		
+	}
+}
