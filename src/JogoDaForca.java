17d1bf0a6512e15fd24ef7119de4daa0db802606
Angêlica Araújo <134542537+araujo-angel@users.noreply.github.com>
2024-05-24 16:48:17 -0300

    atualizacoes nos metodos

--- src/JogoDaForca.java
+++ src/JogoDaForca.java
@@ -4,81 +4,105 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class JogoDaForca {
-	private ArrayList <String> listaDePalavras;
-	private ArrayList<String> listaDasDicas;
-	private ArrayList<Integer> ocorrencias; 
+	private ArrayList<String> listaDePalavras = new ArrayList<>();
+	private ArrayList<String> listaDasDicas = new ArrayList<>();
+	private ArrayList<Integer> ocorrencias = new ArrayList<>();
 	private String palavraSorteada;
 	private String dicaDaPalavraSorteada;
 	private int tentativas;
 	private int acertos;
-	
+
 	public JogoDaForca() throws Exception {
 		InputStream stream = this.getClass().getResourceAsStream("/dados/palavras.txt");
 		if (stream == null)
 			throw new Exception("Arquivo com dados inexistente");
 		Scanner arquivo = new Scanner(stream);
-		
+
 		String linha;
 		while (arquivo.hasNext()) {
 			linha = arquivo.nextLine();
 			this.listaDePalavras.add(linha.split(";")[0]);
-			this.listaDasDicas.add(linha.split(";")[0]);
+			this.listaDasDicas.add(linha.split(";")[1]);
 		}
+		arquivo.close();
 	}
 
-	public void iniciar(){
+	public void iniciar() {
 		tentativas = 0;
 		Random random = new Random();
 		int indiceSorteado = random.nextInt(listaDePalavras.size());
 		palavraSorteada = listaDePalavras.get(indiceSorteado);
 		dicaDaPalavraSorteada = listaDasDicas.get(indiceSorteado);
 	}
-	
+
 	public String getDica() {
 		String dica;
 		if (tentativas == 0 || terminou())
-			return dica = "nao ha dica"; 
-		else 
 			return dicaDaPalavraSorteada;
+		else
+			return dica = "nao ha dica";
 	}
-	
+
 	public int getTamanho() {
 		return palavraSorteada.length();
 	}
-	
-	/*
-	public ArrayList<Integer> getOcorrencias(String letra) throws Exception{}*/
-	
+
+	public ArrayList<Integer> getOcorrencias(String letra) throws Exception{
+		if(letra == "" || letra.length() > 1) {
+			throw new Exception("Entrada invalida. Digite novamente.");}
+		
+		tentativas++;
+		for(int i=0; i<=palavraSorteada.length(); i++)
+			if (palavraSorteada.contains(letra)) {
+				acertos ++;
+				ocorrencias.add(i);
+				return ocorrencias;}
+		else
+			getNomePenalidade();
+			return ocorrencias; 
+			}
+
 	public boolean terminou() {
-		if( acertos == tentativas)
+		if (tentativas == 6)
 			return true;
 		else
 			return false;
 	}
-	
+
 	public String getPalavraAdivinhada() {
 		return palavraSorteada;
 	}
-	
+
 	public int getAcertos() {
 		return acertos;
 	}
-	
-	/*
-	public int getNumeroPenalidade() {}
-	
-	public String getNomePenalidade() {}
-	
-	*/
-	
+
+	public int getNumeroPenalidade() {
+		return tentativas;
+	}
+
+	public String getNomePenalidade() {
+		if (tentativas == 1)
+			return "perdeu a perna esquerda";
+		if (tentativas == 2)
+			return "perdeu a perna direita";
+		if (tentativas == 3)
+			return "perdeu o braço esquerdo";
+		if (tentativas == 4)
+			return "perdeu o braço direito";
+		if (tentativas == 5)
+			return "perdeu o tronco";
+		else
+				return "perdeu a cabeça";
+	}
+
 	public String getResultado() {
 		if (!terminou())
 			return "jogo em andamento";
+		else if (acertos == tentativas) // ver se a logica esta certa
+			return "voce venceu";
 		else
-			if ( acertos == tentativas) //ver se a logica esta certa
-				return "voce venceu";
-			else
-				return "voce foi enforcado";
+			return "voce foi enforcado";
 	}
 
 }
